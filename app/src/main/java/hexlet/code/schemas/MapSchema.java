5d2ccaccc18ef026e4757bee176d104b4142f858
SergeiMed <s.medvedev86@mail.ru>
2022-11-09 23:58:22 +0300

    refactor MapSchema NumberSchema

---
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -0,0 +1,30 @@
+package hexlet.code.schemas;
+
+import hexlet.code.BaseSchema;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+public final class MapSchema extends BaseSchema<Map<String, Object>> {
+
+    public Predicate<Map<String, Object>> isNotEmpty() {
+        return Objects::nonNull;
+    }
+
+    public MapSchema required() {
+        checkList.add(isNotEmpty());
+        return this;
+    }
+
+    public MapSchema sizeof(int minSize) {
+        checkList.add(x -> x.size() >= minSize);
+        return this;
+    }
+
+    public MapSchema shape(Map<String, BaseSchema> map) {
+        checkList.add(x -> x.keySet().stream()
+                .allMatch(q -> map.get(q).isValid(x.get(q))));
+        return this;
+    }
+}
