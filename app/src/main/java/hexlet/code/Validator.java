1504dd9b544e2325e9b7331c885cf2ace409fa74
SergeiMed <s.medvedev86@mail.ru>
2022-11-09 23:57:21 +0300

    fix validator

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -1,5 +1,9 @@
 package hexlet.code;
 
+import hexlet.code.schemas.MapSchema;
+import hexlet.code.schemas.NumberSchema;
+import hexlet.code.schemas.StringSchema;
+
 public class Validator {
 
     public final StringSchema string() {
@@ -8,5 +12,9 @@ public class Validator {
 
     public final NumberSchema number() {
         return new NumberSchema();
+    }
+
+    public final MapSchema map() {
+        return new MapSchema();
     }
 }
