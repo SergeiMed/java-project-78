11809775abc22e956e7d5ebc5a8b40227c143982
SergeiMed <s.medvedev86@mail.ru>
2022-11-30 00:22:26 +0300

    fix NumberSchema

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -8,12 +8,12 @@ public final class NumberSchema extends BaseSchema {
     }
 
     public NumberSchema positive() {
-        addCheck(value -> (int) value > 0);
+        addCheck(value -> ((int) value) > 0);
         return this;
     }
 
     public NumberSchema range(int beginNumber, int endNumber) {
-        addCheck(value -> beginNumber <= (int) value && (int) value <= endNumber);
+        addCheck(value -> beginNumber <= ((int) value) && ((int) value) <= endNumber);
         return this;
     }
 }
